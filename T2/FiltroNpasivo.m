61f88555cfd9476f6158b683eef019bf859b234e
cisternashdiego <37970600+cisternashdiego@users.noreply.github.com>
2018-07-09 09:48:03 -0400

    hacer la tarea 2 como sea

--- T2/FiltroNpasivo.m
+++ T2/FiltroNpasivo.m
@@ -1,17 +1,14 @@
 clear all
 clc
 
-n=3
-Rs=1
-Ls=0.1
-i1ef=1
-inef=0.4
+m=3
+Ls=0.00012
+i1ef=(538.48)/(2^0.5)
+inef=(163.35)/(2^0.5)
 w=100*pi
-Kn=0.1
-Q=100
+Kn=0.05
+Q=1000
 
-Rn=(Kn*i1ef/(inef^2 -(Kn*i1ef)^2))*(Rs*Kn*i1ef+sqrt(((Rs*inef)^2)+((n*w*Ls)^2)*((inef^2)-(Kn^2)*(i1ef^2))))
-Ln=(Rn*Q)/(n*w)
-Cn=1/((n^2)*(w^2)*Ln)
-
-
+Rn=(Kn*i1ef/(inef^2 -(Kn*i1ef)^2))*(sqrt(((m*w*Ls)^2)*((inef^2)-(Kn^2)*(i1ef^2))))
+Ln=(Rn*Q)/(m*w)
+Cn=1/((m^2)*(w^2)*Ln)
